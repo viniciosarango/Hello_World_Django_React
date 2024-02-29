cd6a4db6a15c8f20dc31f0c4d520682a717cb983
viniciosarango <viniciosarango23@gmail.com>
2024-02-29 18:29:16 -0500

    aumentamos !!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 Esto es un repositorio basico para aprender git y github
-Que me permite conocer los comnados basicos
+Que me permite conocer los comnados basicos.
+
+# git y github
+
+![Git y GitHub] (img/entrenamiento-personalizado_)
+
+Sigo modificando mi cambio.
--- hello_World.py
+++ hello_World.py
@@ -1,6 +1,6 @@
 def welcome_message():
     name = input ("Ingrese su nombre: ")
-    print("Bienvenido", name, "al curso de Dajango y React!")
+    print("Bienvenido", name, "al curso de Dajango y React, estamos jueves 29 de febrero!!!")
 
 if __name__ == "__main__":
     welcome_message()
